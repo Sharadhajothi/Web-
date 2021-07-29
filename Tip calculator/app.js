78f558924aef7dd4f6d55e0b3c907bd8a907d9ac
Sharadha Jothilingam <62168805+Sharadhajothi@users.noreply.github.com>
2021-07-29 15:08:54 +0530

    Update app.js
    Same as v1

--- Tip calculator/app.js
+++ Tip calculator/app.js
@@ -1,4 +1,4 @@
-(function () {
+/*(function () {
     'use strict'
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -16,7 +16,7 @@
                 form.classList.add('was-validated')
             }, false)
         })
-})()
+})()*/
 
 var calculate = document.getElementById('calculate');
 
